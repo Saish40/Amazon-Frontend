e0365a0e729a346222c9ecfc8803dd429599dd36
Saish <saishpatil9@gamil.com>
2023-10-27 00:28:03 +0530

    Added files

--- Jenkins script/main.tf
+++ Jenkins script/main.tf
@@ -1,28 +1,28 @@
 resource "aws_security_group" "Jenkins-SG" {
-    name = "Jenkins-SG"
-    description = "This is security group for Jenkins"
+  name        = "Jenkins-SG"
+  description = "This is security group for Jenkins"
 
 
-    ingress = [
-        for port in [22, 443, 80, 8080, 9000, 3000] : {
-            description      = "TLS from VPC"
-            from_port        = port
-            to_port          = port
-            protocol         = "tcp"
-            cidr_blocks      = ["0.0.0.0/0"]
-            ipv6_cidr_blocks = []
-            prefix_list_ids  = []
-            security_groups  = []
-            self             = false
-  }
-]
-
-    egress {
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
+  ingress = [
+    for port in [22, 443, 80, 8080, 9000, 3000] : {
+      description      = "TLS from VPC"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
     }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "Jenkins-SG"
@@ -30,12 +30,18 @@ resource "aws_security_group" "Jenkins-SG" {
 }
 
 resource "aws_instance" "Amazon" {
-    ami = "ami-0fc5d935ebf8bc3bc"
-    instance_type = "t2.large"
-    vpc_security_group_ids = [ aws_security_group.Jenkins-SG.id ]
+  ami                    = "ami-0fc5d935ebf8bc3bc"
+  instance_type          = "t2.large"
+  vpc_security_group_ids = [aws_security_group.Jenkins-SG.id]
+  user_data              = templatefile("./install_jenkins.sh", {})
 
-    tags = {
-      Name = "Amazon"
-    }
-  
+  root_block_device {
+    volume_size = 30
+  }
+
+  tags = {
+    Name = "Amazon"
+  }
+
+
 }
